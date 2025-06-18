9fdb3333dc4fd98ccbd33d003fb29c0017da24c4
Delorenzo6000 <ramon.delorenzo@yahoo.com>
2025-06-18 23:22:39 +0100

    Updates

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Service name
-  serviceName: 'Service name goes here',
+  serviceName: 'NHS Wales - DHCW Design System',
 
   // Port to run nodemon on locally
   port: 2000,
